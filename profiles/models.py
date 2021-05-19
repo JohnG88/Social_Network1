6e6282f479191fb4278fc9e37f6d596acd01f059
John Gutierrez <djterez88@gmail.com>
2021-05-19 16:08:10 -0700

    Added signal for many to many accepted status, both profiles can see each other in friends list

--- profiles/models.py
+++ profiles/models.py
@@ -22,7 +22,7 @@ class Profile(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username}-{self.created}"
+        return f"{self.user.username}-{self.created.strftime('%d-%m-%Y')}"
     
     # Creating first profile with name and last name in slug, the following profiles, if they have the same name will have a number by them, prob how red eyed coder did with implementing the time to slug(maybe it was pyplane?)
     def save(self, *args, **kwargs):
@@ -48,5 +48,17 @@ class Profile(models.Model):
         super().save(*args, **kwargs)
 
 
+STATUS_CHOICES = (
+    ('send', 'send'),
+    ('accepted', 'accepted'),
+)
+
 class Relationship(models.Model):
-    
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender}-{self.receiver}-{self.status}"
